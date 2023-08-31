c2bd0881e8cc8e8672f8c5afe11691a299f48aa2
ice breaker <1324318532@qq.com>
2023-08-31 16:18:40 +0800

    chore: commit escape case

--- packages/escape/test/babel.test.ts
+++ packages/escape/test/babel.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape, prettier/prettier */
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import babel from '@babel/core'
@@ -5,6 +6,17 @@ import MagicString from 'magic-string'
 import { jsStringEscape } from '@/index'
 
 describe('babel', () => {
+  it('escape', () => {
+    const arr = ['"\'', '\"\'', "\"'", "\"\'"]
+    for (let i = 0; i < arr.length; i++) {
+      const a = arr[i];
+      for (let j = i + 1; j < arr.length; j++) {
+        const b = arr[j];
+        console.log(a, b)
+        expect(a).toBe(b)
+      }
+    }
+  })
   it('case0', async () => {
     const code = await fs.readFile(path.resolve(__dirname, './fixtures/case0.js'), 'utf8')
     const s = new MagicString(code)
@@ -35,5 +47,43 @@ describe('babel', () => {
     const ms = s.toString()
     expect(ms).toMatchSnapshot()
     await fs.writeFile(path.resolve(__dirname, './fixtures/case0.out.js'), ms, 'utf8')
+    expect(ms).toEqual(code)
+  })
+
+  it('case1', async () => {
+    const code = await fs.readFile(path.resolve(__dirname, './fixtures/case1.js'), 'utf8')
+    const s = new MagicString(code)
+    const result = ['\'1\'\\n\\t', '2\\n"34"']
+    let idx = 0
+    await babel.transformAsync(code, {
+      plugins: [
+        {
+          visitor: {
+            StringLiteral(p) {
+              if (p.node.start && p.node.end && p.node.start > -1 && p.node.end > -1) {
+                const v = s.slice(p.node.start + 1, p.node.end - 1)
+                expect(v).toBe('\\\'"\\"12\\n34')
+                expect(p.node.value).toBe('\'""12\n34')
+                s.update(p.node.start + 1, p.node.end - 1, jsStringEscape(p.node.value))
+              }
+            },
+            TemplateElement(p) {
+              if (p.node.start && p.node.end && p.node.start > -1 && p.node.end > -1) {
+                const v = s.slice(p.node.start, p.node.end)
+                expect(v).toBe(result[idx])
+                expect(p.node.value.raw).toBe(result[idx])
+                s.update(p.node.start, p.node.end, v) // jsStringEscape(v))
+                idx++
+              }
+            }
+          }
+        }
+      ]
+    })
+    const ms = s.toString()
+    expect(ms).toMatchSnapshot()
+    await fs.writeFile(path.resolve(__dirname, './fixtures/case1.out.js'), ms, 'utf8')
+    // expect(ms).toEqual(code)
+    expect('\'"\"12\n34').toBe('\'\"\"12\n34')
   })
 })
